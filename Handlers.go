367638fe5e7feb3924b2c9b88ab5133d32eef838
Mark Rofail <markm.rofail@gmail.com>
2017-11-01 22:02:02 +0200

    feat: add listhandler

--- Handlers.go
+++ Handlers.go
@@ -1,49 +1,72 @@
 package main
 
 import (
-    "encoding/json"
-    "fmt"
-    "net/http"
-    "github.com/gorilla/mux"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
 
-    "github.com/satori/go.uuid"
-    
+	"github.com/gorilla/mux"
+	"github.com/satori/go.uuid"
+	"golang.org/x/net/context"
+	"googlemaps.github.io/maps"
 )
 
 func Index(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintln(w, "Welcome!")
+	fmt.Fprintln(w, "Welcome!")
 }
 
 func TodoIndex(w http.ResponseWriter, r *http.Request) {
-    todos := Todos{
-        Todo{Name: "Write presentation"},
-        Todo{Name: "Host meetup"},
-    }
+	todos := Todos{
+		Todo{Name: "Write presentation"},
+		Todo{Name: "Host meetup"},
+	}
 
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-    w.WriteHeader(http.StatusOK)
-	
-    if err := json.NewEncoder(w).Encode(todos); err != nil {
-        panic(err)
-    }
+	w.WriteHeader(http.StatusOK)
+
+	if err := json.NewEncoder(w).Encode(todos); err != nil {
+		panic(err)
+	}
 }
 
 func TodoShow(w http.ResponseWriter, r *http.Request) {
-    vars := mux.Vars(r)
-    todoId := vars["todoId"]
-    fmt.Fprintln(w, "Todo show:", todoId)
+	vars := mux.Vars(r)
+	todoId := vars["todoId"]
+	fmt.Fprintln(w, "Todo show:", todoId)
 }
 
 func WelcomeHandler(w http.ResponseWriter, r *http.Request) {
 
-    welcomeMessage := WelcomeStruct{
-        Message:"Welcome ,where do you want to go ?",
-        Uuid: uuid.NewV1(),
-    }
+	welcomeMessage := WelcomeStruct{
+		Message: "Welcome ,where do you want to go ?",
+		Uuid:    uuid.NewV1(),
+	}
 
-    if err := json.NewEncoder(w).Encode(welcomeMessage); err != nil {
-        panic(err)
-    }
-    
+	if err := json.NewEncoder(w).Encode(welcomeMessage); err != nil {
+		panic(err)
+	}
 
+}
+
+func ListHandler(w http.ResponseWriter, r *http.Request) {
+	c, err := maps.NewClient(maps.WithAPIKey("AIzaSyBZwHSODUVFhzMcAEabT-BOw2_SkOrYEWo"))
+	if err != nil {
+		log.Fatalf("fatal error: %s", err)
+	}
+
+	req := &maps.NearbySearchRequest{
+		Location: &maps.LatLng{Lat: 29.985352, Lng: 31.279194},
+		RankBy:   "distance",
+		Keyword:  "resturants",
+	}
+
+	res, err := c.NearbySearch(context.Background(), req)
+	if err != nil {
+		log.Fatalf("fatal error: %s", err)
+	}
+
+	if err := json.NewEncoder(w).Encode(res); err != nil {
+		panic(err)
+	}
 }
