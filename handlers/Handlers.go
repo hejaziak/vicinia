962c84bd65ffd59eb822ef84900ce3f2042a44b4
Ather Hejazi <Hejazi@Athers-MacBook-Pro.local>
2017-11-03 18:59:19 +0200

    feat: api.ai respond messages

--- handlers/Handlers.go
+++ handlers/Handlers.go
@@ -11,11 +11,11 @@ import (
 	global "vicinia/globals"
 	structures "vicinia/structures"
 
-	"github.com/kamalpy/apiai-go"
 	"github.com/kr/pretty"
 	"github.com/satori/go.uuid"
 	"golang.org/x/net/context"
 	"googlemaps.github.io/maps"
+	"github.com/marcossegovia/apiai-go"
 )
 
 func WelcomeHandler(w http.ResponseWriter, r *http.Request) {
@@ -53,37 +53,53 @@ func ChatHandler(w http.ResponseWriter, r *http.Request) {
 func getList(w http.ResponseWriter, r *http.Request, uuid uuid.UUID, message string) {
 	c := global.GetMapClient()
 
-	ai := apiaigo.APIAI{
-		AuthToken: "71027bbaf70a4a53847bedce6b83c94f",
-		Language:  "en-US",
-		SessionID: uuid.String(),
-	}
+	client, err := apiai.NewClient(
+        &apiai.ClientConfig{
+            Token:      "71027bbaf70a4a53847bedce6b83c94f",
+            QueryLang:  "en",    //Default en
+            SpeechLang: "en-US", //Default en-US
+        },
+    )
+    if err != nil {
+        fmt.Printf("%v", err)
+    }
+    //Set the query string and your current user identifier.
+    qr, err := client.Query(apiai.Query{Query: []string{message}, SessionId: uuid.String()})
+    if err != nil {
+        fmt.Printf("%v", err)
+    }
+    action := qr.Result.Action
 
-	resp, err := ai.SendText(message)
+    if(strings.Compare(action,"search") != 0 ){
+		if err := json.NewEncoder(w).Encode(qr.Result.Fulfillment.Speech); err != nil {
+			panic(err)
+		}  	
 
-	keyword := resp.Result.Parameters["keyword"]
-	fmt.Println(keyword)
+    } else {
+    	
+	    keyword := qr.Result.Params["keyword"]
 
-	req := &maps.NearbySearchRequest{
-		Location: &maps.LatLng{Lat: 29.985352, Lng: 31.279194},
-		RankBy:   "distance",
-		Keyword:  keyword,
-	}
+		req := &maps.NearbySearchRequest{
+			Location: &maps.LatLng{Lat: 29.985352, Lng: 31.279194},
+			RankBy:   "distance",
+			Keyword:  string(keyword.(string)),
+		}
 
-	res, err := c.NearbySearch(context.Background(), req)
-	if err != nil {
-		log.Fatalf("fatal error: %s", err)
-	}
+		res, err := c.NearbySearch(context.Background(), req)
+		if err != nil {
+			log.Fatalf("fatal error: %s", err)
+		}
 
-	output := SimplifyList(res.Results)
+		output := SimplifyList(res.Results)
 
-	jsonMessage, _ := json.Marshal(output)
+		jsonMessage, _ := json.Marshal(output)
 
-	respondMessage := extractMessage(string(jsonMessage), "To get detailed information about a specific place, please type its ID")
-	if err := json.NewEncoder(w).Encode(respondMessage); err != nil {
-		panic(err)
-	}
-	updateSession(extractUUID(r), res.Results)
+		respondMessage := extractMessage(string(jsonMessage), "To get detailed information about a specific place, please type its ID")
+		if err := json.NewEncoder(w).Encode(respondMessage); err != nil {
+			panic(err)
+		}
+		updateSession(extractUUID(r), res.Results)
+    }
 }
 
 func extractUUID(r *http.Request) uuid.UUID {
